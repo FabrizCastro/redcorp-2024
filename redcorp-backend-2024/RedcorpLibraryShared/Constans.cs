9ba41c2bfc11dab01f3b60605ea8fbe498a2db93
FabrizCastro <fabrizziocastro2612@gmail.com>
2024-05-01 00:57:40 -0500

    Add Constans

---
+++ redcorp-backend-2024/RedcorpLibraryShared/Constans.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RedcorpLibraryShared
+{
+    public class Constans
+    {
+        public const int MaxQuantityInventory = 250;
+        public const string SecretKey = "my_redcorp_center_upc_secret_key";
+
+    }
+}
